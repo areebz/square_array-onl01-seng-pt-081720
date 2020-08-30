bc4d0c01e487533d63753949f184f6d9990cb3c0
Areeba Zahir <areebazahir3@gmail.com>
2020-08-30 05:58:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,8 @@
-def square_array(numbers)
- ary = [1,2,3,4,5]
-ary.each do |i|
-   puts i
+def square_array(array)
+  new_array = []
+  array.each{|a| new_array.push(a*a)}
+  return new_array
 end
+
+arr = [1, 2, 3, 4, 5]
+square_array(arr)
